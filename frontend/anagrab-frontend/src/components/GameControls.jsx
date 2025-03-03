8ac6f28f296c652f994951b93a3167e2d7e07665
Shameer Shahab <shameershahab@gmail.com>
2025-03-03 04:02:40 -0500

    banana emoji

--- frontend/anagrab-frontend/src/components/GameControls.jsx
+++ frontend/anagrab-frontend/src/components/GameControls.jsx
@@ -4,14 +4,9 @@ export const GameControls = ({ onFlipLetter, deckCount }) => {
       <div className="buttons">
         <button onClick={onFlipLetter} className="bag-button" title="Flip Letter">
           <div className="bag-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="bag-svg"
-            >
-              <path d="M4 4c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v2H4V4zm2 6V6h12v4H6zm-2 0h16v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V10z" />
-            </svg>
+            <span role="img" aria-label="banana" className="bag-svg">
+              🍌
+            </span>
             <span className="bag-count">{deckCount || 0}</span>
           </div>
         </button>
